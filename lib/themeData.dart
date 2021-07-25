4740567036761c0e98699cfcbd90eb335150a667
rpo <hakimnaufal212@gmail.com>
2021-07-25 19:18:58 +0700

    theme data for app

---
+++ lib/themeData.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static const TextStyle regular = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 25,
+  );
+  static const TextStyle bold = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 25,
+    fontWeight: FontWeight.bold,
+  );
+}
