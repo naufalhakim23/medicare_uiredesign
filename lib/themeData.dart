d8ff4555ca87ebd50df5cee3e17239b17df435a1
rpo <hakimnaufal212@gmail.com>
2021-07-26 12:24:44 +0700

    adding hexcolor converter and more textstyle and color constance

--- lib/themeData.dart
+++ lib/themeData.dart
@@ -2,14 +2,35 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   AppTheme._();
+  static const Color notWhite = Color(0xFFEDF0F2);
+  static const Color grey = Color.fromRGBO(46, 46, 46, 100);
+  static const Color darkGreen = Color.fromRGBO(20, 48, 59, 100);
+  static const Color nearlyWhite = Color(0xFFFFFFFF);
+  static const Color nearlyBlue = Color(0xFF00B6F0);
+  static const TextStyle Greetings = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 25,
+  );
+  static const TextStyle Name = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w900,
+    fontSize: 25,
+  );
+  static const TextStyle Title = TextStyle(
+      fontFamily: 'Poppins',
+      fontSize: 18,
+      fontWeight: FontWeight.w500,
+      color: AppTheme.grey);
+}
 
-  static const TextStyle regular = TextStyle(
-    fontFamily: 'Poppins',
-    fontSize: 25,
-  );
-  static const TextStyle bold = TextStyle(
-    fontFamily: 'Poppins',
-    fontSize: 25,
-    fontWeight: FontWeight.bold,
-  );
+class HexColor extends Color {
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
 }
